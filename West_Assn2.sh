729f6acd127556f3591d2bcbcac89471cb7e70a4
Amy West <acw1471@utulsa.edu>
2020-09-20 15:38:57 -0500

    changed file names and removed them

--- West_Assn2.sh
+++ West_Assn2.sh
@@ -2,12 +2,12 @@
 
 touch child.sh
 
-echo 'cut -f 1-2,4 BTS_data.txt | grep ID > BTS_heading' >> child.sh
-echo 'sort -k1 BTS_data.txt > BTS_sort' >> child.sh
-echo 'sed -n '40,400p' BTS_sort | sort -k4rg | cut -f 1-2,4 > BTS_abridged' >> child.sh
-echo 'cat BTS_abridged >> BTS_heading' >> child.sh
-echo 'column -t BTS_heading > BTS_final' >> child.sh
-
+echo 'cut -f 1-2,4 BTS_data.txt | grep ID > dBTS_heading' >> child.sh
+echo 'sort -k1 BTS_data.txt > dBTS_sort' >> child.sh
+echo 'sed -n '40,400p' dBTS_sort | sort -k4rg | cut -f 1-2,4 > dBTS_abridged' >> child.sh
+echo 'cat dBTS_abridged >> dBTS_heading' >> child.sh
+echo 'column -t dBTS_heading > BTS_final' >> child.sh
+echo 'rm dBTS*' >> child.sh
 
 chmod u+x child.sh
 
