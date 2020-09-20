c156189dbf18e089cea2f7463beac0ba4777d107
Amy West <acw1471@utulsa.edu>
2020-09-20 15:23:47 -0500

    redirected files

--- West_Assn2.sh
+++ West_Assn2.sh
@@ -4,7 +4,7 @@ touch child.sh
 
 echo 'cut -f 1-2,4 BTS_data.txt | grep ID > BTS_heading' >> child.sh
 echo 'sort -k1 BTS_data.txt > BTS_sort' >> child.sh
-echo 'sed -n '17,218p' BTS_sort | sort -k4rg | cut -f 1-2,4 > BTS_abridged' >> child.sh
+echo 'sed -n '40,400p' BTS_sort | sort -k4rg | cut -f 1-2,4 > BTS_abridged' >> child.sh
 echo 'cat BTS_abridged >> BTS_heading' >> child.sh
 echo 'column -t BTS_heading > BTS_final' >> child.sh
 
